e16d0082a9877b411d53c3316451dba840df9a10
swaffelen <rappinpasha@gmail.com>
2020-07-31 19:59:10 +0400

    set components export/import & webpack cfg added

---
+++ src/utils/render.js
@@ -0,0 +1,3 @@
+export const render = (container, position, text) => {
+  container.insertAdjacentHTML(position, text);
+};
---
+++ src/view/films-list-extra.js
@@ -0,0 +1,9 @@
+export const createFilmsListExtraTemplate = (header) => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">${header}</h2>
+
+      <div class="films-list__container">
+    </section>`
+  );
+};
