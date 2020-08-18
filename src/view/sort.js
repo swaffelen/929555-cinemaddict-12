12afc2a0f62a33e6afd36595a89bae7a9ab03a6f
swaffelen <rappinpasha@gmail.com>
2020-08-18 15:41:20 +0400

    more refactoring

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,9 +1,11 @@
 import AbstractView from "./abstract.js";
 import {SortType} from "../consts.js";
 
+const activeSortClass = `sort__button--active`;
+
 const createSortTemplate = () => {
   return `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.BY_DEFAULT}">Sort by default</a></li>
+      <li><a href="#" class="sort__button ${activeSortClass}" data-sort-type="${SortType.BY_DEFAULT}">Sort by default</a></li>
       <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_DATE}">Sort by date</a></li>
       <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_RATING}">Sort by rating</a></li>
     </ul>`;
@@ -28,9 +30,9 @@ export default class Sort extends AbstractView {
     this._callback.click(evt.target.dataset.sortType);
     this.getElement()
       .querySelectorAll(`a`)
-      .forEach((filter) => filter.classList.remove(`sort__button--active`));
+      .forEach((filter) => filter.classList.remove(activeSortClass));
 
-    evt.target.classList.add(`sort__button--active`);
+    evt.target.classList.add(activeSortClass);
   }
 
   setClickHandler(callback) {
